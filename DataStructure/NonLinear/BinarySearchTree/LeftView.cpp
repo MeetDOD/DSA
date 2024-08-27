04eccd3508f86661cb8e3c53fcae53168fa02b25
meet <dodiyameet041@gmail.com>
2024-08-27 22:59:17 +0530

    Added left view

--- DataStructure/NonLinear/BinarySearchTree/LeftView.cpp
+++ DataStructure/NonLinear/BinarySearchTree/LeftView.cpp
@@ -1,81 +1,109 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node * left;
-        Node * right;
-    Node(int value){
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
         data = value;
         left = NULL;
         right = NULL;
     }
 };
 
-Node * BuildTree(){
-    
-    int data;
-    cin >> data;
-    
-    if(data == -1){
+Node *binaryTree()
+{
+
+    int x;
+    cin >> x;
+
+    if (x == -1)
+    {
         return NULL;
     }
-    
-    Node * temp = new Node(data);
-    
-    cout<<"Enter the data for left Node : ";
-    temp -> left = BuildTree();
-    
-    cout<<"Enter the data for right Node : ";
-    temp -> right = BuildTree();
-    
+
+    Node *temp = new Node(x);
+
+    // left
+    cout << "Enter the left child of " << x << " : ";
+    temp->left = binaryTree();
+
+    // right
+    cout << "Enter the right child of " << x << " : ";
+    temp->right = binaryTree();
+
     return temp;
 }
 
-void InOrder(Node * root){
-    
-    if(root == NULL){
-        return;
-    }    
-    
-    InOrder(root -> left);
-    cout<< root -> data <<" ";
-    InOrder(root -> right);
-}
-
-void solve(Node * root, vector <int> &ans, int level){
-    if(root == NULL){
+void inOrder(Node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
-    
-    if(ans.size() == level)
-    ans.push_back(root -> data);
-    solve(root -> left,ans,level + 1);
-    solve(root -> right,ans, level + 1);
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
 }
 
-void LeftView(Node * root){
-    vector <int> ans;
-    solve(root,ans,0);
-    
-    for(int i = 0 ; i < ans.size() ; i ++){
-        cout<< ans[i] << " ";
+void leftView(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    vector<int> ans;
+
+    while (!q.empty())
+    {
+        ans.push_back(q.front()->data);
+
+        int n = q.size();
+
+        while (n--)
+        {
+            Node *temp = q.front();
+            q.pop();
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
     }
 }
 
-int main() {
-    
-    cout<<"Enter the root Node : ";
-    Node * root = NULL;
-    root = BuildTree();
-    
-    InOrder(root);
-    
-    cout<<endl;
-    
-    LeftView(root);
-    
+int main()
+{
+
+    cout << "Enter the root node : ";
+    Node *root;
+    root = binaryTree();
+
+    cout << "Inorder : ";
+    inOrder(root);
+
+    cout << endl;
+
+    cout << "Left View : ";
+    leftView(root);
+
     return 0;
 }
