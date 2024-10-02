f35cfbec74583b3f826086acc2dc5f58ff80aa59
meet <dodiyameet041@gmail.com>
2024-10-02 19:27:23 +0530

    Added Pattern Question and Answers

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -3,26 +3,15 @@ using namespace std;
 
 int main()
 {
-    // 1 2 3 4 5
-    // 1 2 3 4 5
-    // 1 2 3 4 5
-    // 1 2 3 4 5
-    // 1 2 3 4 5
 
-    int n;
-    cin >> n;
-
-    int i = 1;
-
-    while (i <= n)
+    for (int i = 1; i < 5; i++)
     {
-        int j = 1;
-        while (j <= n)
+        for (int j = 1; j < 5; j++)
         {
-            cout << j;
-            j = j + 1;
+            cout << i << " ";
         }
         cout << endl;
-        i = i + 1;
     }
+
+    return 0;
 }
