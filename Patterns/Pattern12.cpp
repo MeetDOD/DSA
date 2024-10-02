668d4b5da7b81052df88381aec011c3e2a375880
meet <dodiyameet041@gmail.com>
2024-10-02 22:55:41 +0530

    Added Pattern Question and Answers also added output

--- Patterns/Pattern12.cpp
+++ Patterns/Pattern12.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 
-    for (int i = 1; i < 6; i++)
+    for (int i = 1; i < 5; i++)
     {
-        for (int j = i; j >= 1; j--)
+        for (int j = 1; j <= i; j++)
         {
             char name = 'a' + i - 1;
             cout << name << " ";
