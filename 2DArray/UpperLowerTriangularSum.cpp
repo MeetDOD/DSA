de8d3a2ae3be73a3d22236f780376dca4e7a8fc0
meet <dodiyameet041@gmail.com>
2024-10-26 06:24:16 +0530

    Modifyied the upper lower matrix problem solution

--- 2DArray/UpperLowerTriangularSum.cpp
+++ 2DArray/UpperLowerTriangularSum.cpp
@@ -8,7 +8,18 @@ int main()
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}};
-
+        
+    cout<<"Original Matrix"<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+    
+    cout<<endl;
+    
     int lower_sum = 0;
     int upper_sum = 0;
 
@@ -17,19 +28,23 @@ int main()
         for (int j = 0; j <= i; j++)
         {
             lower_sum = lower_sum + arr[i][j];
-        }
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
     }
+    
+    cout << "Lower Triangular sum is : "<<lower_sum << endl;
+    cout<<endl;
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 3 - 1; j >= i; j--)
         {
             upper_sum = upper_sum + arr[i][j];
-        }
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
     }
 
-    cout << lower_sum << endl;
-    cout << upper_sum << endl;
+    cout << "Upper Triangular sum is : "<<upper_sum << endl;
 
     return 0;
 }
