18f0606813e61eb10749c6b169519869c790263a
meet <dodiyameet041@gmail.com>
2024-11-06 23:46:13 +0530

    Added TCS number problem

--- TCS/Numbers/AllPrimePrint.cpp
+++ TCS/Numbers/AllPrimePrint.cpp
@@ -2,30 +2,36 @@
 #include <math.h>
 using namespace std;
 
-bool isPrime(int n){
-    
-    for(int i = 2 ; i <= sqrt(n) ; i++){
-        if(n % i == 0){
+bool isPrime(int n)
+{
+
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int main() {
-    
+int main()
+{
+
     int number1;
     int number2;
-    
-    cout<<"Enter the start number : ";
+
+    cout << "Enter the start number : ";
     cin >> number1;
-    
-    cout<<"Enter the end number : ";
+
+    cout << "Enter the end number : ";
     cin >> number2;
-    
-    for(int i = number1 ; i <= number2 ; i++){
-        if(isPrime(i)){
-            cout<< i <<" ";
+
+    for (int i = number1; i <= number2; i++)
+    {
+        if (isPrime(i) && i != 1)
+        {
+            cout << i << " ";
         }
     }
 
