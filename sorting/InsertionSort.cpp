20b5093d3f7f97a7d3ed9bfedd51b23265ad0816
meet <dodiyameet041@gmail.com>
2024-12-16 19:21:43 +0530

    Added insertion sort algorithm

---
+++ sorting/InsertionSort.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> arr = {5, 4, 3, 3, 2, 3, 1};
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+
+        int temp = arr[i];
+
+        int j = i - 1;
+
+        for (; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
